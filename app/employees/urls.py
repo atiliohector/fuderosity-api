7825e35720bec6d2505c1d6b16b9b393ba99fbb0
Atilio <atiliogoulart@gmail.com>
2021-06-06 12:46:45 -0300

    Updating routes

--- app/employees/urls.py
+++ app/employees/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = (
-    url(r'^employees/$', views.EmployeeViewSet.as_view(), name='employees'),
+    url('employees/', views.EmployeeViewSet.as_view(), name='employees'),
 )

a327165009c1e530d5e91f84b522df11b123ad05
Atilio <atiliogoulart@gmail.com>
2021-06-06 12:39:18 -0300

    Update restframework configs

--- app/settings.py
+++ app/settings.py
@@ -31,7 +31,10 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
     'PAGE_SIZE': 10
 }
 

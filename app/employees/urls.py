7f956e8aec582f3e2efd61ef05757e41794cbfb8
Atilio <atiliogoulart@gmail.com>
2021-06-07 17:25:13 -0300

    Adding GET view

--- app/employees/urls.py
+++ app/employees/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = (
-    url('employees/', views.beautiful_employees, name='employees'),
+    url('employees/', views.fuderosity_employees, name='employees'),
 )

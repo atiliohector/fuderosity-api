16443db1b519e3b0fa96bbe850eeb1ab26bf0792
Atilio <atiliogoulart@gmail.com>
2021-06-07 17:20:19 -0300

    Updating configs

--- app/employees/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import generics
-
-from .serializer import EmployeesSerializer
-from .models import EmployeeModel
-
-class EmployeesViews(generics.ListCreateAPIView):
-    queryset = EmployeeModel.objects.all()
-    serializer_class = EmployeesSerializer

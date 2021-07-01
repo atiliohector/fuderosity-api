06b5448819b75caf8ec7848ad1a3908ac4cd0589
Atilio <atiliogoulart@gmail.com>
2021-07-01 18:29:26 -0300

    1.5

--- app/managers/views.py
+++ app/managers/views.py
@@ -17,3 +17,22 @@ class TodoListApiView(APIView):
         todos = ManagersModel.objects.all()
         serializer = ManagerSerializer(todos, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        '''
+        Create the Todo with given todo data
+        '''
+        data = {
+            'user': request.user.id,
+            'name': request.data.get('name'), 
+            'age': request.data.get('age'), 
+            'position': request.data.get('position'),
+            'guild': request.data.get('guild')
+            
+        }
+        serializer = ManagerSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

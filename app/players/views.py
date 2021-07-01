71b42f661845939a376f7049dbbf7320c396bd10
Atilio <atiliogoulart@gmail.com>
2021-07-01 18:24:13 -0300

    1.1

--- app/players/views.py
+++ app/players/views.py
@@ -1,21 +1,39 @@
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
 
 from .models import PlayersModels
 from .serializer import PlayersSerializer
 
 
-class CreatePlayers(generics.CreateAPIView):
-    queryset = PlayersModels.objects.all()
-    serializer_class = PlayersSerializer
+class TodoListApiView(APIView):
+    # add permission to check if user is authenticated
 
-class AllPlayers(generics.ListAPIView):
-    queryset = PlayersModels.objects.all()
-    serializer_class = PlayersSerializer
+    # 1. List all
+    def get(self, request, *args, **kwargs):
+        '''
+        List all the todo items for given requested user
+        '''
+        todos = PlayersModels.objects.all()
+        serializer = PlayersSerializer(todos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-class UpdatePlayers(generics.UpdateAPIView):
-    queryset = PlayersModels.objects.all()
-    serializer_class = PlayersSerializer
+    def post(self, request, *args, **kwargs):
+        '''
+        Create the Todo with given todo data
+        '''
+        data = {
+            'user': request.user.id,
+            'name': request.data.get('name'), 
+            'age': request.data.get('age'), 
+            'position': request.data.get('position'),
+            'guild': request.data.get('guild')
+            
+        }
+        serializer = PlayersSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-class DeletePlayers(generics.DestroyAPIView):
-    queryset = PlayersModels.objects.all()
-    serializer_class = PlayersSerializer
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

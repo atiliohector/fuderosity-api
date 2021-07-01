ab3d5e6e896fefdaf62240761835a8092c67d538
Atilio <atiliogoulart@gmail.com>
2021-07-01 18:27:36 -0300

    1.2

--- app/managers/views.py
+++ app/managers/views.py
@@ -1,20 +1,4 @@
-from rest_framework import generics
 
 from .models import ManagersModel
 from .serializer import ManagerSerializer
 
-class CreateManager(generics.CreateAPIView):
-    queryset = ManagersModel.objects.all()
-    serializer_class = ManagerSerializer
-
-class AllManagers(generics.ListAPIView):
-    queryset = ManagersModel.objects.all()
-    serializer_class = ManagerSerializer
-
-class UpdateManagers(generics.UpdateAPIView):
-    queryset = ManagersModel.objects.all()
-    serializer_class = ManagerSerializer
-
-class DeleteManagers(generics.DestroyAPIView):
-    queryset = ManagersModel.objects.all()
-    serializer_class = ManagerSerializer

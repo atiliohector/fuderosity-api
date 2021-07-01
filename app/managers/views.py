79e133fad4350894ce823c22d7985153f2e1eb14
Atilio <atiliogoulart@gmail.com>
2021-07-01 18:28:33 -0300

    1.4

--- app/managers/views.py
+++ app/managers/views.py
@@ -6,3 +6,14 @@ from rest_framework import permissions
 from .models import ManagersModel
 from .serializer import ManagerSerializer
 
+class TodoListApiView(APIView):
+    # add permission to check if user is authenticated
+
+    # 1. List all
+    def get(self, request, *args, **kwargs):
+        '''
+        List all the todo items for given requested user
+        '''
+        todos = ManagersModel.objects.all()
+        serializer = ManagerSerializer(todos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)

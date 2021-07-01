596d6ac0431bb7d8cd8ee203355371ade6dab491
Atilio <atiliogoulart@gmail.com>
2021-07-01 18:29:59 -0300

    1.6

--- app/managers/urls.py
+++ app/managers/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     
-    path('create/', views.CreateManager.as_view(),name='create_managers'),
-    path('', views.AllManagers.as_view(),name='all_managers'),
-    path('delete/<int:id>/', views.DeleteManagers.as_view(),name='delete_manager'),
-    path('update/<int:id>/',views.UpdateManagers.as_view(), name='update_manager'),
+    path('', views.TodoListApiView.as_view()),
+
 ]

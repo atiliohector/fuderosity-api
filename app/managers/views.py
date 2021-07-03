3423623ebd1d2443bd0bef2481a92753c2099eff
Atilio <atiliogoulart@gmail.com>
2021-07-02 22:25:06 -0300

    1.5

--- app/managers/views.py
+++ app/managers/views.py
@@ -1,31 +1,13 @@
-from rest_framework.views import APIView
+from rest_framework.view import APIView
 from rest_framework.response import Response
 
 from .models import ManagersModel
 from .serializer import ManagerSerializer
-from app.managers import serializer
 
 class AllManagers(APIView):
 
     def get(self, request):
 
         managers = ManagersModel.objects.all()
-        serializer = ManagerSerializer(managers, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-
-        data = {
-            'name': request.data.get('name'),
-            'age': request.data.get('age'),
-            'champions': request.data.get('champions'),
-            'champion_name': request.data.get('champion_name'),
-        }
-
-        serializer = ManagerSerializer(data=data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.erros)
+        managers_serializer = ManagerSerializer(managers, many=True)
+        return Response(managers_serializer.data)

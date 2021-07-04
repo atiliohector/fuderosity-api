60e7a6fb2c9bf8aa3045687d8fc51b73f45d7357
Atilio <atiliogoulart@gmail.com>
2021-07-04 11:16:15 -0300

    1.1

--- app/managers/urls.py
+++ app/managers/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
 
     path('', views.AllManagers.as_view()),
+    path('create/', views.AddManager.as_view()),
+    path('manager/<int:id>/', views.ManagerById.as_view())
 
 ]
--- app/managers/views.py
+++ app/managers/views.py
@@ -1,32 +1,49 @@
+from django.http.response import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import ManagersModel
 from .serializer import ManagerSerializer
 
+
 class AllManagers(APIView):
 
     def get(self, request):
 
         managers = ManagersModel.objects.all()
-        serializer = ManagerSerializer(managers, many=True)
-        return Response(serializer.data)
+        manager_serializer = ManagerSerializer(managers, many=True)
+        return Response(manager_serializer.data)
+
+class AddManager(APIView):
 
     def post(self, request):
 
         data = {
 
-            'name':  request.data.get('name'),
-            'age':  request.data.get('age'),
-            'champions':  request.data.get('champions'),
-            'champion_name':  request.data.get('champion_name'),
-
+            'name': request.data.get('name'),
+            'age': request.data.get('age'),
+            'champions': request.data.get('champions'),
+            'champion_name': request.data.get('champion_name'),
         }
 
-        serializer = ManagerSerializer(data=data)
+        manager_serializer = ManagerSerializer(data=data)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+        if manager_serializer.is_valid():
+            manager_serializer.save()
+            return Response(manager_serializer.data)
         else:
-            return Response(serializer.erros)
+            return Response(manager_serializer.errors)
+
+class ManagerById(APIView):
+
+    def get_object(self, id):
+        try:
+            return ManagersModel.objects.get(id=id)
+        except ManagersModel.DoesNotExist:
+            return Http404
+
+    def get(self, request, id, format=None):
+        
+        manager = self.get_object(id=id)
+        manager_serializer = ManagerSerializer(manager)
+        return Response(manager_serializer.data)

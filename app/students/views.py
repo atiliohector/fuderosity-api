9f90c0617916307c4c43e0ae83427a28e8272aff
Atilio <atiliogoulart@gmail.com>
2021-07-16 16:37:17 -0300

    1.6 Updating views

--- app/students/views.py
+++ app/students/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import StudentModel
+from .serializer import StudentSerializer
+
+class AllStudent(APIView):
+
+    def get(self, request):
+
+        student_model = StudentModel.objects.all()
+        student_serializer = StudentSerializer(student_model, many=True)
+        return Response(student_serializer.data)

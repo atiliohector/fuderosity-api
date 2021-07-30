f581d60dd83fd7dd0f1a72e7498a83ee20a1f2cb
Atilio <atiliogoulart@gmail.com>
2021-07-30 10:46:25 -0300

    Update user models

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class UserModel(models.Model):
+
+    name = models.CharField(max_length=255, blank=True, null=True)
+    age = models.IntegerField(default=18, blank=True, null=True)
+
+
+    def __str__(self):
+        return self.name
